8b9218a427cbc14f9b307b4ae53234326389c8d3
qpooqp777 <qpooqp777@gmail.com>
2022-06-23 13:57:20 +0800

    0623-1357

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -4,18 +4,12 @@ export default function PolicyPage() {
   return (
     <Layout>
       <p>
-        This is an example site to demonstrate how to use{" "}
+        This is an example site to demonstrate how to use
         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
       </p>
       <h2>Terms of Service</h2>
       <p>
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        test
       </p>
       <h2>Privacy Policy</h2>
       <p>
